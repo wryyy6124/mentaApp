df5e416932492ed0835fe486b4a9346a65d58cc5
ktamu <wryyy.6124@gmail.com>
2021-10-14 09:46:28 +0900

    211014_vue-practice_main-branch

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,32 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <p v-if="testFlg">Flg is OK</p>
+    <p v-if="!testFlg">Flg is NG</p>
+    <p>{{ msg }}</p>
+    <button v-on:click="clickTest">切り替え</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'Apple',
+  data: function() {
+    return {
+      msg: 'Test First Message',
+      testFlg: false,
+    }
+  },
+  methods: {
+    clickTest: function() {
+      if(this.testFlg) {
+        this.msg = 'This is NG Msg';
+        this.testFlg = false;
+      } else {
+        this.msg = 'This is OK Msg';
+        this.testFlg = true;
+      }
+    },
+  }
 }
 </script>
 
