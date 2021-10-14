2a1a5fd6b9d1877b2f8503fb5ade386e786ae7c4
ktamu <wryyy.6124@gmail.com>
2021-10-14 21:41:54 +0900

    211014_02_vue-calc-function_main-branch

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,31 @@
 <template>
   <div id="app">
-    <p v-if="testFlg">Flg is OK</p>
-    <p v-if="!testFlg">Flg is NG</p>
-    <p>{{ msg }}</p>
-    <button v-on:click="clickTest">切り替え</button>
+    <div class="selectArea">
+      <button class="selectArea__button" v-on:click="btnClickCalc">プラス</button>
+      <button class="selectArea__button" v-on:click="btnClickCalc">マイナス</button>
+      <button class="selectArea__button" v-on:click="btnClickCalc">初期化</button>
+    </div>
+    <div class="resultArea">
+      <dt class="resultArea__header">結果</dt>
+      <dd class="resultArea__body">{{ result }}</dd>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Apple',
+  name: 'App',
   data: function() {
     return {
-      msg: 'Test First Message',
-      testFlg: false,
+      result: 0,
     }
   },
   methods: {
-    clickTest: function() {
-      if(this.testFlg) {
-        this.msg = 'This is NG Msg';
-        this.testFlg = false;
-      } else {
-        this.msg = 'This is OK Msg';
-        this.testFlg = true;
-      }
+    btnClickCalc: function(e) {
+      // 押下したボタンのテキストで計算方法を判定・処理を実行する
+      e.target.innerText === 'プラス' ? this.result++ : '';
+      e.target.innerText === 'マイナス' ? this.result-- : '';
+      e.target.innerText === '初期化' ? this.result = 0 : '';
     },
   }
 }
@@ -32,11 +33,42 @@ export default {
 
 <style>
 #app {
+  color: #2c3e50;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
+
+.selectArea,
+.resultArea {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 auto;
+  width: 500px;
+}
+
+.selectArea { margin-bottom: 30px; }
+
+.selectArea__button {
+  cursor: pointer;
+  padding: 10px 0;
+  width: 31%;
+}
+
+.resultArea { font-size: 20px; }
+
+.resultArea__header,
+.resultArea__body { width: 49%; }
+
+.resultArea__header { text-align: right; }
+
+.resultArea__body {
+  border: 1px solid #666;
+  text-align: center;
+  padding: 10px;
+}
+
 </style>
