932e363d69049a08d3f6249f85f0556fb73412f7
ktamu <wryyy.6124@gmail.com>
2021-10-25 23:18:43 +0900

    211025_vue-props_middle01_main-branch

--- src/components/componentTask4.vue
+++ src/components/componentTask4.vue
@@ -8,18 +8,18 @@
         <ul class="dataGive__body__list">
           <li>
             <span class="dataGive__list__header">商品名</span>
-            <input class="dataGive__list__inputText"　type="text" name="Product" placeholder="商品名">
+            <input class="dataGive__list__inputText"　type="text" v-model.trim="name" name="productName" placeholder="商品名称を入力" autocomplete="off">
           </li>
           <li>
             <span class="dataGive__list__header">詳細</span>
-            <input class="dataGive__list__inputText"　type="text" name="Product" placeholder="商品詳細">
+            <input class="dataGive__list__inputText"　type="text" v-model.trim="detail" name="productDetail" placeholder="商品の詳細を入力" autocomplete="off">
           </li>
           <li>
             <span class="dataGive__list__header">金額</span>
-            <input class="dataGive__list__inputText"　type="text" name="Product" placeholder="商品金額">
+            <input class="dataGive__list__inputText"　type="number" v-model.trim="price" name="productPrice" placeholder="金額は1円以上の数値を入力" autocomplete="off">
           </li>
         </ul>
-        <div class="dataGive__body__append">
+        <div class="dataGive__body__append" @click="onClickDataAppend">
           追加
         </div>
       </div>
@@ -27,30 +27,22 @@
 
     <div class="dataProduct">
       <ol class="dataProduct__list">
-        <li>
-          <div class="dataProduct__image">
-            Dummy
-          </div>
+        <li v-for="(item, index) in lists" :key="index">
+          <div class="dataProduct__image">Dummy</div>
           <div class="dataProduct__body">
-            <div class="dataProduct__header">
-              サンプル商品
-            </div>
-            <div class="dataProduct__text">
-              こちらはサンプル商品です
-            </div>
-            <div class="dataProduct__price">
-              &yen; 1,200
-            </div>
+            <div class="dataProduct__name">{{ item.name }}</div>
+            <div class="dataProduct__detail">{{ item.detail }}</div>
+            <div class="dataProduct__price"><span>&yen;</span>{{ item.price }}</div>
             <div class="dataProduct__button">
               <div class="dataProduct__add">Add to Cart</div>
-              <div class="dataProduct__delete">Delete</div>
+              <div class="dataProduct__delete" @click="onClickListDelete(index)">Delete</div>
             </div>
           </div>
         </li>
       </ol>
     </div>
 
-    <div class="dataResult">
+    <div class="dataResult" v-if="result.display">
       <div class="dataResult__header">
         合計
       </div>
@@ -70,12 +62,76 @@
 <script>
 export default {
   name: 'componentTask4',
+  props: {
+    products : {
+      // 名前
+      name: [String, Number],
+      // 詳細
+      detail: [String, Number],
+      // 価格
+      price: Number,
+    }
+  },
   data: function() {
     return {
+      // 親コンポーネント（App.vue）から渡ってきた配列を格納
+      lists: this.products,
 
+      //各商品情報を格納する
+      product: {},
+
+      // 名前
+      name: '',
+      // 詳細
+      detail: '',
+      // 価格
+      price: '',
+
+      // カートに入れた商品の合計金額
+      result: {
+        total: 0,
+        display: false,
+      }
     }
   },
   methods: {
+    onClickDataAppend: function() {
+      // 各テキストフィールドに1字以上の入力がされているか
+      if (this.name !== '' && this.detail !== '' && this.price !== '') {
+        if (this.price <= 0) {
+          alert('金額は1円以上の数値を入力してください');
+          return;
+        }
+
+        // 入力内容をオブジェクトへ格納
+        this.product = {
+          name: this.name,
+          detail: this.detail,
+          price: Number(this.price).toLocaleString(),
+        }
+
+        // 配列にオブジェクト（各入力内容）を追加
+        this.lists.push(this.product);
+
+        // 合計金額部分を活性化
+        this.result.display = true;
+
+        // 入力内容の初期化
+        this.name = '';
+        this.detail = '';
+        this.price = '';
+      }
+    },
+
+    onClickListDelete: function(num) {
+      this.lists.splice(num, 1);
+      console.log(this.lists.length);
+
+      if (this.lists.length === 0) {
+        // リストが空だったら合計金額部分を非活性化
+        this.result.display = false;
+      }
+    },
   },
   computed: {
   },
@@ -157,34 +213,43 @@ export default {
       flex-basis: 31%;
 
       &:not(:nth-of-type(3n)) { margin-right: 3.5%; }
-      &:not(:nth-last-of-type(n+3)) { margin-bottom: 0; }
+      &:last-of-type { margin-right: 0; }
+
+      &:not(:nth-of-type(-n+3)) { margin-bottom: 0; }
     }
   }
 
   &__image {
     background-color: #666;
+    border-radius: 5px 5px 0 0;
     color: #fff;
     text-align: center;
     padding: 50px 0;
   }
 
   &__body {
-    padding: 15px 5px 5px 5px;
+    padding: 15px 10px;
     > div:not(:last-of-type) { margin-bottom: 20px; }
   }
 
-  &__header, &__text {
+  &__name, &__detail {
     line-height: 1.5;
     padding-left: 10px;
   }
 
-  &__text { font-size: 75%; }
+  &__detail { font-size: 75%; }
 
-  &__header, &__price { font-weight: bold; }
+  &__name, &__price { font-weight: bold; }
 
   &__price {
     color: #df0000;
     text-align: center;
+
+    > span {
+      font-size: 80%;
+      display: inline-block;
+      margin-right: 5px;
+    }
   }
 
   &__button {
@@ -200,7 +265,7 @@ export default {
       text-align: center;
       transition: .4s;
       padding: 10px 5px;
-      flex-basis: 49%;
+      flex-basis: 48%;
 
       &:hover { opacity: .5; }
     }
